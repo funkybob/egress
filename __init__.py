56d42a47332a639b4e99934a77b59e7ef9782f4e
Curtis <curtis@tinbrain.net>
2014-03-13 00:31:52 +1100

    Implement connect function

--- __init__.py
+++ __init__.py
@@ -3,14 +3,23 @@ from .exceptions import *
 from .connection import *
 from .types import *
 
+from . import libpq
+
 # Module Interface
 
-def connect(*args, **kwargs):
+def connect(**kwargs):
     '''
     Constructor for creating a connection to the database.
 
-    Returns a Connection Object. It takes a number of parameters which are database dependent.
+    Returns a Connection Object. It takes a number of parameters which are
+    database dependent.
     '''
+    conn_str = ' '.join([
+        '='.join(item)
+        for item in kwargs.items()
+    ])
+    conn = libpq.PQconnectdb(conn_str)
+    return Connection(conn, **kwargs)
 
 apilevel = '2.0'
 
