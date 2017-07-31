191cda51e1dd7dd2e584a66a48b35baea76fc46b
Curtis Maloney <curtis@tinbrain.net>
2017-07-31 22:49:06 +1000

    Raise the right exception for the error code

--- egress/connection.py
+++ egress/connection.py
@@ -1,8 +1,79 @@
 
 from . import libpq
 from .cursor import Cursor
-from .exceptions import DatabaseError
+from . import exceptions
 
+
+EXC_MAP = {
+    # Class 0A - Feature Not Supported
+    b'0A': exceptions.NotSupportedError,
+
+    # Class 21 - Cardinality Violation
+    b'21': exceptions.ProgrammingError,
+    # Class 22 - Data Exception
+    b'22': exceptions.DataError,
+    # Class 23 - Integrity Constraint Violation
+    b'23': exceptions.IntegrityError,
+    # Class 24 - Invalid Cursor State
+    b'24': exceptions.InternalError,
+    # Class 25 - Invalid Transaction State
+    b'25': exceptions.InternalError,
+    # Class 26 - Invalid SQL Statement Name
+    b'26': exceptions.OperationalError,
+    # Class 27 - Triggered Data Change Violation
+    b'27': exceptions.OperationalError,
+    # Class 28 - Invalid Authorization Specification
+    b'28': exceptions.OperationalError,
+    # Class 2B - Dependent Privilege Descriptors Still Exist
+    b'2B': exceptions.InternalError,
+    # Class 2D - Invalid Transaction Termination
+    b'2D': exceptions.InternalError,
+    # Class 2F - SQL Routine Exception
+    b'2F': exceptions.InternalError,
+
+    # Class 34 - Invalid Cursor Name
+    b'34': exceptions.OperationalError,
+    # Class 38 - External Routine Exception
+    b'38': exceptions.InternalError,
+    # Class 39 - External Routine Invocation Exception
+    b'39': exceptions.InternalError,
+    # Class 3B - Savepoint Exception
+    b'3B': exceptions.InternalError,
+    # Class 3D - Invalid Catalog Name
+    b'3D': exceptions.ProgrammingError,
+    # Class 3F - Invalid Schema Name
+    b'3F': exceptions.ProgrammingError,
+
+    # Class 40 - Transaction Rollback
+    b'40': exceptions.ProgrammingError,  # TransactionRollbackError,
+    # Class 42 - Syntax Error or Access Rule Violation
+    b'42': exceptions.ProgrammingError,
+    # Class 44 - WITH CHECK OPTION Violation
+    b'44': exceptions.ProgrammingError,
+
+    # b'55': exceptions.QueryCanceledError,
+    # b'57': exceptions.QueryCanceledError,
+    # b'50': exceptions.QueryCanceledError,
+    # b'51': exceptions.QueryCanceledError,
+    # b'54': exceptions.QueryCanceledError,
+    # Class 53 - Insufficient Resources
+    b'53': exceptions.OperationalError,
+    # Class 54 - Program Limit Exceeded
+    b'54': exceptions.OperationalError,
+    # Class 55 - Object Not In Prerequisite State
+    b'55': exceptions.OperationalError,
+    # Class 57 - Operator Intervention
+    b'57': exceptions.OperationalError,
+    # Class 58 - System Error (errors external to PostgreSQL itself)
+    b'58': exceptions.OperationalError,
+
+    # Class F0 - Configuration File Error
+    b'F0': exceptions.InternalError,
+    # Class P0 - PL/pgSQL Error
+    b'P0': exceptions.InternalError,
+    # Class XX - Internal Error
+    b'X': exceptions.InternalError,
+}
 
 class Connection(object):
     def __init__(self, PGconn, **kwargs):
@@ -92,7 +163,7 @@ class Connection(object):
         if status == libpq.CONNECTION_OK:
             return
         msg = libpq.PQerrorMessage(self.conn)
-        raise DatabaseError(msg)
+        raise exceptions.DatabaseError(msg)
         # Raise appropriate error
 
     def _check_cmd_result(self, result):
@@ -100,5 +171,11 @@ class Connection(object):
         if status in (libpq.PGRES_COMMAND_OK, libpq.PGRES_TUPLES_OK):
             return self._check_status()
         msg = libpq.PQresultErrorMessage(result)
+        msg = msg.decode('utf-8').strip()
+
+        code = libpq.PQresultErrorField(result, libpq.PG_DIAG_SQLSTATE)
+
+        exc_class = EXC_MAP.get(code[:2], exceptions.DatabaseError)
+
         libpq.PQclear(result)
-        raise DatabaseError(msg)
+        raise exc_class(msg)
