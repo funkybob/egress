86f21ab1c3cbcf67788bc94d3942c3527f654ba7
Vidya Rani D G <vidya@travelanalytics.com.au>
2017-08-07 15:39:17 +1000

    Do not override master's eegres settings file

--- egress/django/settings_egress.py
+++ egress/django/settings_egress.py
@@ -16,18 +16,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'egress.django',
         'NAME': 'django_test',
-        'USER': 'postgres',
-        'PASSWORD': 'SlimShady96',
-        'HOST': 'localhost',
-        'PORT': '',
     },
     'other': {
         'ENGINE': 'egress.django',
         'NAME': 'django_test',
-        'USER': 'postgres',
-        'PASSWORD': 'SlimShady96',
-        'HOST': 'localhost',
-        'PORT': '',
     }
 }
 
