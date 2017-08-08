8f57f623ae0fbcb111d04432353aa5f29d9633e6
Vidya Rani D G <vidya@travelanalytics.com.au>
2017-08-08 10:20:19 +1000

    First test to verify inserting numeric type

--- egress/tests/test_numeric.py
+++ egress/tests/test_numeric.py
@@ -8,8 +8,14 @@ import egress as db
 from egress.tests.config import DATABASE
 from egress.tests.utils import create_db, drop_db
 
+db_name = 'test_egress_db'
 
-def connect():
+
+def connect(dbname=None):
+    '''Connect to a postgres server, if dbname then connect to the database'''
+    if db_name:
+        DATABASE['dbname'] = dbname
+
     return db.connect(**DATABASE)
 
 
@@ -18,7 +24,7 @@ def setUpModule():
     connection = connect()
     connection._autocommit = True
     with connection.cursor() as cursor:
-        create_db(cursor)
+        create_db(cursor, db_name)
 
 
 def tearDownModule():
@@ -26,14 +32,33 @@ def tearDownModule():
     connection = connect()
     connection._autocommit = True
     with connection.cursor() as cursor:
-        drop_db(cursor)
-
-    connection.close()
+        drop_db(cursor, db_name)
 
 
 class TestNumericTypes(unittest.TestCase):
     '''
     Tests for postgres numeric types
     '''
-    def test_numeric(self):
-        self.skipTest('TBD')
+    def execute_operation_on_db(self, operation):
+        connection = connect(dbname=db_name)
+        connection._autocommit = True
+        with connection.cursor() as cursor:
+            cursor.execute(operation)
+            return cursor.rowcount
+
+    def setUp(self):
+        create_table = """
+        CREATE TABLE bookings (best_price numeric(10,2))
+        """
+        self.execute_operation_on_db(create_table)
+
+    def tearDown(self):
+        create_table = """DROP TABLE bookings"""
+        self.execute_operation_on_db(create_table)
+
+    def test_insert_numeric_ok(self):
+        insert_numeric = """
+        INSERT INTO bookings (best_price) VALUES (1000.02)
+        """
+        result = self.execute_operation_on_db(insert_numeric)
+        self.assertEqual(result, 1)
